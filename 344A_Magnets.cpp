4b93878ec15fa85b9f6c6b22be1b2ac550186324
SimoneG-Coder <simone22012006@gmail.com>
2026-07-24 22:09:15 +0530

    Create 344A_Magnets.cpp

---
+++ 344A_Magnets.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string prev, curr;
+    int groups = 1; // first magnet starts a group
+    cin >> prev;
+    for (int i = 1; i < n; i++) {
+        cin >> curr;
+        if (curr != prev) {
+            groups++;
+        }
+        prev = curr;
+    }
+    cout << groups << endl;
+    return 0;
+}
