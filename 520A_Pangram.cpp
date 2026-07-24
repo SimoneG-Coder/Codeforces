5353d322bde2bb87ad5722c16d4822eec6a536d8
SimoneG-Coder <simone22012006@gmail.com>
2026-07-24 22:14:57 +0530

    Create 520A_Pangram.cpp

---
+++ 520A_Pangram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    set<char> letters;
+    for (char c : s) {
+        letters.insert(tolower(c));
+    }
+
+    if (letters.size() == 26) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
